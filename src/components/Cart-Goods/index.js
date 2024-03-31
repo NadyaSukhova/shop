016db67f266ecf5265a65196694e5a2376cc5d78
Nadya Sukhova <suhova1313@gmail.com>
2024-03-31 22:16:55 +0300

    addded payment form

--- src/components/Cart-Goods/index.js
+++ src/components/Cart-Goods/index.js
@@ -1,7 +1,8 @@
-import { memo } from "react";
+import { memo, useEffect, useState } from "react";
 import cn from "../../utils/classnames.ts";
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 import Good from "../Good/index.js";
+import Payment from "../Payment/index.js";
 import "./style.css";
 
 function CartGoods({
@@ -13,37 +14,44 @@ function CartGoods({
 }) {
   const { t } = useTranslation();
   const className = "Cart-Goods";
+  const [showPayment, setShowPayment] = useState(false);
+  const [payment, setPayment] = useState(<></>);
+
+  useEffect(() => {
+    if (showPayment)
+      setPayment(<Payment close={() => setShowPayment(false)} cost={cart.price.toLocaleString("ru")} />);
+    else setPayment(<></>);
+  });
   return (
-    <div className={className}>
-      
-      <div className={cn(className, "title")}>
-        {cart.goods.length != 0
-          ? t('cart')
-          : t('cart.empty')}
-      </div>
-          <Good
-            dispatchAction={dispatchAction}
-            addToCart={addToCart}
-            deleteFromCart={deleteFromCart}
-            deleteGoodCart={deleteGoodCart}
-            cart={cart}
-          />
-          <div className={cn(className, "panel")}>
-        <div className={cn(className, "panel-total")}>
-          <p>{t('cart.total').toUpperCase()}</p>{" "}
-          <p>₽ {cart.price.toLocaleString("ru")}</p>{" "}
+    <>
+      {payment}
+      <div className={className}>
+        <div className={cn(className, "title")}>
+          {cart.goods.length != 0 ? t("cart") : t("cart.empty")}
         </div>
-        <div
-          className={cn(className, "panel-button")}
-          onClick={() => {
-            window.location =
-              "https://www.sberbank.com/ru/person/remittance/beznal_cc_rus";
-          }}
-        >
-         {t('cart.confirm')}
+        <Good
+          dispatchAction={dispatchAction}
+          addToCart={addToCart}
+          deleteFromCart={deleteFromCart}
+          deleteGoodCart={deleteGoodCart}
+          cart={cart}
+        />
+        <div className={cn(className, "panel")}>
+          <div className={cn(className, "panel-total")}>
+            <p>{t("cart.total").toUpperCase()}</p>{" "}
+            <p>₽ {cart.price.toLocaleString("ru")}</p>{" "}
+          </div>
+          <div
+            className={cn(className, "panel-button")}
+            onClick={() => {
+              setShowPayment(true)
+            }}
+          >
+            {t("cart.confirm")}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
---
+++ src/components/Payment/index.js
@@ -0,0 +1,105 @@
+import { memo, useEffect } from "react";
+import cn from "../../utils/classnames.ts";
+import "./style.css";
+
+function Payment({ cost, close }) {
+  const className = "Payment";
+
+  const formatNumber = (number) =>
+    number.split("").reduce((seed, next, index) => {
+      if (index !== 0 && !(index % 4)) seed += " ";
+      return seed + next;
+    }, "");
+
+  const formatDate = (number) =>
+    number.split("").reduce((seed, next, index) => {
+      if (index !== 0 && !(index % 2)) seed += "/";
+      return seed + next;
+    }, "");
+
+  useEffect(() => {
+    const cardInput = document.getElementById("card-number");
+    cardInput.addEventListener(
+      "input",
+      () =>
+        (cardInput.value = formatNumber(cardInput.value.replaceAll(" ", "")))
+    );
+    const dateInput = document.getElementById("expiry-date");
+    dateInput.addEventListener(
+      "input",
+      () => (dateInput.value = formatDate(dateInput.value.replaceAll("/", "")))
+    );
+    const codeInput = document.getElementById("security-code");
+    codeInput.onkeydown = function (event) {
+      if (isNaN(event.key) && event.key !== "Backspace") {
+        event.preventDefault();
+      }
+    };
+  });
+
+  return (
+    <div className={className}>
+      <form className={cn(className, "form")}>
+        <div>
+          <label for="name">Name on card</label>
+          <input
+            required
+            type="text"
+            id="name"
+            name="name"
+            autocomplete="cc-name"
+          />
+        </div>
+        <div class="card-number">
+          <label>Card number</label>
+          <input
+            required
+            type="text"
+            id="card-number"
+            name="card-number"
+            placeholder="0000 0000 0000 0000"
+            inputmode="numeric"
+            autcomplete="cc-number"
+            minlength="19"
+            maxlength="19"
+            pattern="[0-9]{16}"
+          />
+        </div>
+        <div class="date-code">
+          <div>
+            <label for="expiry-date">Expiry date</label>
+            <input
+              required
+              type="text"
+              id="expiry-date"
+              name="expiry-date"
+              class="expiry-date"
+              autocomplete="cc-exp"
+              placeholder="MM/YY"
+              minlength="5"
+              maxlength="5"
+              pattern="[0-9/]{4}"
+            />
+          </div>
+          <div>
+            <label for="security-code">Security code</label>
+            <input
+              required
+              type="text"
+              id="security-code"
+              autocomplete="cc-number"
+              name="security-code"
+              inputmode="numeric"
+              minlength="3"
+              maxlength="3"
+              pattern="[0-9]{3}"
+            />
+          </div>
+        </div>
+        <button>Оплатить {cost} ₽</button>
+        <button onClick={close}>Отмена</button>
+      </form>
+    </div>
+  );
+}
+export default memo(Payment);
