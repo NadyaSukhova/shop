fdac06266d4a4554e4beedf693c20ffd9aa1886b
Nadya Sukhova <suhova1313@gmail.com>
2024-03-31 19:34:51 +0300

    fixed mistake in addToCart func

--- src/store/cart/actions.js
+++ src/store/cart/actions.js
@@ -1,30 +1,33 @@
 export function addToCart(item) {
-  document.getElementById('but' + item.id).style.visibility="visible";
-  setTimeout(() => {document.getElementById('but' + item.id).style.visibility="hidden"}, 50)
-    return {
-      type: 'add-to-cart / sucess',
-      price: item.price,
-      id: item.id,
-      title: item.title,
-      img: item.img,
-    };
+  if (document.getElementById("but" + item.id)) {
+    document.getElementById("but" + item.id).style.visibility = "visible";
+    setTimeout(() => {
+      document.getElementById("but" + item.id).style.visibility = "hidden";
+    }, 50);
   }
-  
-  export function deleteFromCart(item) {
-    return {
-      type: 'delete-from-cart / sucess',
-      price: item.price,
-      id: item.id,
-      title: item.title,
-      img: item.img,
-    };
-  }
+  return {
+    type: "add-to-cart / sucess",
+    price: item.price,
+    id: item.id,
+    title: item.title,
+    img: item.img,
+  };
+}
 
-  export function deleteGoodCart(id, price) {
-    return {
-      type: 'delete-good-cart / sucess',
-      id: id,
-      price: price,
-    };
-  }
-  
+export function deleteFromCart(item) {
+  return {
+    type: "delete-from-cart / sucess",
+    price: item.price,
+    id: item.id,
+    title: item.title,
+    img: item.img,
+  };
+}
+
+export function deleteGoodCart(id, price) {
+  return {
+    type: "delete-good-cart / sucess",
+    id: id,
+    price: price,
+  };
+}
